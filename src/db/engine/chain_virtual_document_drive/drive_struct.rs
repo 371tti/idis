de0bdc6993a8a6feb3df545b45f81a6da6f97a9f
371tti <109227273+371tti@users.noreply.github.com>
2025-01-09 00:24:17 +0900

    Header構造体のフィールドのデータ型を変更し、MetadataBlock構造体をMetadataにリネームしてフィールドを更新

--- src/db/engine/chain_virtual_document_drive/drive_struct.rs
+++ src/db/engine/chain_virtual_document_drive/drive_struct.rs
@@ -10,10 +10,9 @@ struct Header {
     compression: u16,       // Compression algorithm used
     crypt_method: u16,      // Encryption method used
     nb_snapshots: u32,      // Number of snapshots in the drive
-    total_size: u128,       // Total size of the drive in bytes
-    meta_offset: u128,      // Offset to the meta (index) section in bytes
-    data_offset: u128,      // Offset to the data section in bytes
-    snapshot_offset: u128,  // Offset to the snapshot section in bytes
+    total_size: u64,       // Total size of the drive in bytes
+    meta_offset: u64,      // Offset to the meta (index) section in bytes
+    snapshot_offset: u64,  // Offset to the snapshot section in bytes
     block_size: u64,        // Block size in bytes
     entry_count: u64,       // Number of entries in the drive
 }
@@ -28,26 +27,35 @@ enum CryptMethod {
     Aes256 = 1,
 }
 
-const BLOCK_SIZE: usize = 4096; // 4KBブロック
+#[repr(C)]
+struct Snapshot {
+    id : u128,               // Snapshot ID
+    versions: Vec<u128>,      // List of back drive ids
+}
+// 各バージョンのidのリスト
 
 #[repr(C)]
-struct MetadataBlock {
-    id: u128,                    // ノードID
-    size_in_block: u64,         // ブロック数
-    date_created: u64,          // 作成日時
-    date_modified: u64,         // 更新日時
-    date_accessed: u64,         // 最終アクセス日時
-    parent_id: u128,             // 親ノードID
-    data_offset: u128,           // データの開始オフセット
-    child_count: u32,           // 子ノードの数
-    perm: Vec<u128>,               // ノード名
-    children_offsets: Vec<u128>, // 子ノードのセット
-    name: String,            // ノード名
+struct Metadata {
+    id: u128,                    // ノードID 16byte
+    size: u64,         // サイズ 8byte
+    date_created: u64,          // 作成日時 8byte
+    date_modified: u64,         // 更新日時 8byte
+    date_accessed: u64,         // 最終アクセス日時 8byte
+    parent_offset: u64,             // 親ノードのオフセット 8byte
+    data_offset: u64,           // データの開始オフセット 8byte
+    data_size: u64,             // データのサイズ 8byte
+    child_count: u64,           // 子ノードの数 8byte
+    name: [u8; 256],            // ノード名 256byte
+    perm: Vec<u128>,               // ノードの権限 16n-byte
+    children_offsets: Vec<u64>, // 子ノードのオフセットセット 8m-byte
+    child_snapshot_version: Vec<u32>, // 子ノードのスナップショットバージョン 4m-byte
+}
+// メタデータはブロックに1つ配置していく 容量が足りなくなってきたら重なる 細断化しにくいように余裕を持たせる 必ず連続データになるようにする
+
+#[repr(C)]
+struct DataMeta {
+    id: u128,                    // ノードID 16byte
+    size: u64,         // サイズ 8byte
+    jump_list: Vec<(u64, u64)>,           // ジャンプリスト 16n-byte
 }
 
-struct BlockEntry {
-    block_id: u64,          // ブロックID
-    data_size: u64,         // このブロック内のデータサイズ
-    data_offset: u128,       // データの開始オフセット
-    next_block_id: Option<u64>, // 次のブロックのID（複数ブロックにまたがる場合）
-}
