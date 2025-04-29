3a8d7af1926d9e547cfcfe395cee8ecc9ec16e83
371tti <109227273+371tti@users.noreply.github.com>
2025-04-29 18:33:27 +0900

    CashEntryおよびDriverCash構造体に対する最適化を実施し、非同期読み込み・書き込みメソッドを追加。バッファ管理機能を強化し、キャッシュのクリア機能を実装。新しいバッファファイルを追加。

--- src/main.rs
+++ src/main.rs
@@ -301,6 +301,7 @@ impl CashEntry {
     /// 
     /// # Returns
     /// * `CashEntry` - 確保されたメモリを持つ構造体
+    #[inline]
     pub fn with_size_aligned(size: usize, align: usize) -> Self {
         // リリースビルドで消える
         assert!(align.is_power_of_two(), "Alignment must be a power of 2");
@@ -319,6 +320,7 @@ impl CashEntry {
     /// 
     /// # Returns
     /// * `&mut [u8]` - 可変スライス
+    #[inline]
     pub fn as_mut_slice(&mut self) -> &mut [u8] {
         unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.size) }
     }
@@ -328,18 +330,33 @@ impl CashEntry {
 impl Deref for CashEntry {
     type Target = [u8];
 
+    #[inline]
     fn deref(&self) -> &Self::Target {
         unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.size) }
     }
 }
 
 impl DerefMut for CashEntry {
+    #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.as_mut_slice()
     }
 }
 
+
+impl Clone for CashEntry {
+    fn clone(&self) -> Self {
+        // 新しい領域を確保
+        let mut new_entry = CashEntry::with_size_aligned(self.size, self.align);
+        // 中身をディープコピー
+        new_entry.as_mut_slice().copy_from_slice(&self[..]);
+        new_entry
+    }
+}
+
+
 impl Drop for CashEntry {
+    #[inline]
     fn drop(&mut self) {
         unsafe {
             let layout = Layout::from_size_align(self.size, self.align).unwrap();
@@ -360,6 +377,7 @@ pub struct DriverCash {
 }
 
 impl DriverCash {
+    #[inline]
     pub fn new(file: File, cashed_max_blocks: usize, block_size: u64) -> Self {
         Self {
             cashed_max_blocks,
@@ -371,7 +389,25 @@ impl DriverCash {
     }
 
     /// ブロックを読み込む（キャッシュに無ければファイルから）
-    pub async fn read_block(&mut self, block_pos: u64) -> io::Result<&CashEntry> {
+    #[inline]
+    pub async fn read_block(&mut self, block_pos: u64) -> io::Result<CashEntry> {
+        // キャッシュに存在する場合
+        if self.map.contains(&block_pos) {
+            return Ok(self.map.get(&block_pos).unwrap().clone());
+        }
+
+        let mut entry = CashEntry::with_size_aligned(self.block_size as usize, self.block_size as usize);
+        let mut buf = entry.as_mut_slice();
+        self.file
+            .seek(std::io::SeekFrom::Start(block_pos * self.block_size))
+            .await?;
+        self.file.read_exact(&mut buf).await?;
+
+        Ok(entry)
+    }
+
+    #[inline]
+    pub async fn read_block_cashing(&mut self, block_pos: u64) -> io::Result<&CashEntry> {
         // キャッシュに存在する場合
         if self.map.contains(&block_pos) {
             return Ok(self.map.get(&block_pos).unwrap());
@@ -389,11 +425,13 @@ impl DriverCash {
         Ok(self.map.get(&block_pos).unwrap())
     }
 
+    #[inline]
     pub fn contain(&self, block_pos: u64) -> bool {
         self.map.contains(&block_pos)
     }
 
     /// ブロックを書き込む（キャッシュがあれば更新、ファイルにも即書き込み）
+    #[inline]
     pub async fn write_block(&mut self, block_pos: u64, data: CashEntry) -> io::Result<()> {
         // キャッシュに存在する場合
         if let Some(entry) = self.map.get_mut(&block_pos) {
@@ -409,6 +447,7 @@ impl DriverCash {
 
     /// 強制的にファイルをフラッシュ（整合性のため）
     /// 最適化すべき
+    #[inline]
     pub async fn sync(&mut self) -> io::Result<()> {
         self.write_buf.sort_by_key(|(block_pos, _)| *block_pos);
         for (block_pos, entry) in &self.write_buf {
@@ -421,6 +460,12 @@ impl DriverCash {
         self.file.sync_all().await?;
         Ok(())
     }
+
+    /// キャッシュをクリアする
+    #[inline]
+    pub fn clear(&mut self) {
+        self.map.clear();
+    }
 }
 
 /// キャッシュ構造体
@@ -429,12 +474,15 @@ pub struct Cash {
 }
 
 impl Cash {
+    /// Create new Cash instance
+    /// cashed size is floor(size divided by os_fs_sector_size)
+    #[inline]
     pub async fn new(path: &Path, size: u64) -> Self {
-        #[cfg(target_os = "windows")]
         {
             let dir = path.parent()
                 .unwrap_or_else(|| Path::new(r"I:\"));
-            let sector_size = get_bytes_per_sector(dir).unwrap();
+            let sector_size = get_bytes_per_sector(dir).unwrap() as u64;
+            // サイズをセクタサイズで割る. 切り上げ.
             let cashed_block_size = size / sector_size as u64;
             let file = tokio::fs::OpenOptions::new()
                 .read(true)
@@ -445,11 +493,13 @@ impl Cash {
                 .open(path)
                 .await
                 .unwrap();
-            let driver = DriverCash::new(file, cashed_block_size as usize, sector_size as u64);
+            let driver = DriverCash::new(file, cashed_block_size as usize, sector_size);
             Self { driver }
         }
     }
 
+    /// read data into buffer beginning at position `pos`
+    #[inline]
     pub async fn read(&mut self, buffer: &mut [u8], pos: u64) -> io::Result<()> {
         let mut remaining = buffer.len();
         let mut buf_offset = 0;
@@ -458,7 +508,7 @@ impl Cash {
         while remaining > 0 {
             let block_pos = current_pos / self.driver.block_size;
             let block_offset = (current_pos % self.driver.block_size) as usize;
-            let entry = self.driver.read_block(block_pos).await?;
+            let entry = self.driver.read_block_cashing(block_pos).await?;
             let data = entry.as_ref(); // &[u8] のビュー
             // このブロックからコピーできる長さ
             let to_copy = std::cmp::min(remaining, data.len() - block_offset);
@@ -473,21 +523,94 @@ impl Cash {
 
         Ok(())
     }
-}
+    
+    pub async fn write(&mut self, buffer: &mut [u8], pos: u64) -> io::Result<()> {
+        // 事前計算
+        let len = buffer.len();
+        let first_block_pos = pos / self.driver.block_size;
+        let first_block_offset = (pos % self.driver.block_size) as usize;
+        let mut buffer_seek = 0;
 
+        // 重なり分部をオーバーライド
+        let mut first_block = self.driver.read_block(first_block_pos).await?;
+        let to_copy = std::cmp::min(len, self.driver.block_size as usize - first_block_offset);
+        first_block[first_block_offset..first_block_offset + to_copy].copy_from_slice(&buffer[..to_copy]);
+        buffer_seek += to_copy;
+        self.driver.write_block(first_block_pos, first_block).await?;
+
+        // バッファが1ブロックの書き換えのみなら終了
+        if buffer_seek == len {
+            return Ok(());
+        }
+
+        // ブロックまるまるかきかえるやつを一気に書き込む
+        let mut now_block_pos = (pos + buffer_seek as u64) / self.driver.block_size;
+        while self.driver.block_size <= (len - buffer_seek) as u64 {
+            let mut new_block = CashEntry::with_size_aligned(self.driver.block_size as usize, self.driver.block_size as usize);
+            new_block.copy_from_slice(&buffer[buffer_seek..(buffer_seek + self.driver.block_size as usize)]);
+            self.driver.write_block(now_block_pos as u64, new_block).await?;
+            buffer_seek += self.driver.block_size as usize;
+            now_block_pos += 1;
+        }
+
+        // 余りがなかったら終了
+        if buffer_seek == len {
+            return Ok(());
+        }
+
+        // 後ろの重なり部分をオーバーライド
+        let mut last_block = self.driver.read_block(now_block_pos).await?;
+        last_block.copy_from_slice(&buffer[buffer_seek..len]);
+        self.driver.write_block(now_block_pos, last_block).await?;
+
+        Ok(())
+    }
+
+    pub async fn sync(&mut self) -> io::Result<()> {
+        self.driver.sync().await
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    let size = 1024;
-    let mut cash = Cash::new(Path::new("I:\\RustBuilds\\IDIS\\main\\idis\\lol.bin"), size).await;
-    
 
-    let mut buffer = vec![0u8; 13];
-    if let Err(e) = cash.read(&mut buffer, 0).await {
-        eprintln!("Error reading from cache: {}", e);
-    }
-    
-    println!("Buffer: {:?}", buffer);
-    // ここでキャッシュを使用するコードを追加できます
+    // キャッシュ用ファイルとブロックサイズ
+    let path = Path::new("I:\\RustBuilds\\IDIS\\main\\idis\\lol.bin");
+    // ① まずファイルを開いて……
+    let file = tokio::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)
+        .await.unwrap();
+
+    // ② 16KiB (16384 バイト) に伸ばす
+    file.set_len(16 * 1024).await.unwrap();
+    let block_size = 1024u64;
+    let mut cache = Cash::new(path, block_size).await;
+
+    // 読み込み用バッファ
+    let mut buffer = vec![0u8; block_size as usize];
+
+    // 1 回目の読み込み（ファイルから）
+    let t1 = Instant::now();
+    cache.read(&mut buffer, 0).await.unwrap();
+    let d1 = t1.elapsed();
+    println!("First read took: {:?}", d1);
+
+    // 2 回目の読み込み（キャッシュから）
+    let t2 = Instant::now();
+    cache.read(&mut buffer, 0).await.unwrap();
+    let d2 = t2.elapsed();
+    println!("Second read took: {:?}", d2);
+
+    // 文字列を1回だけ書き込むテスト
+    let mut data = "こんにちわ世界 私たちの目的は異常なオブジェクトの収容と保護です。".as_bytes().to_vec();
+    cache.write(&mut data, 0).await.unwrap();
+    cache.sync().await.unwrap();
+
+    // 書き込んだ内容を読み込んで確認
+    let mut buf = vec![0u8; data.len()];
+    cache.read(&mut buf, 0).await.unwrap();
+    println!("Read data: {:?}", String::from_utf8_lossy(&buf));
 }
